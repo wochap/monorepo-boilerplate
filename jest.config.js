873c8e996fb8aaae6b2b2dffd57584d2c00b0a4b
Gean <gean.marroquin@gmail.com>
2019-08-12 08:03:48 -0500

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@ const commonConfig = require('./jest.common.config')
 module.exports = {
   ...commonConfig,
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
+    ...commonConfig.transform,
     '^.+\\.jsx?$': 'babel-jest',
   },
   projects: ['<rootDir>/packages/*'],
